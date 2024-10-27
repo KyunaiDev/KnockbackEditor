69cda8ac3b3c3dd97dc1d26fad018a5150e6529d
KyunaiDev <sofleydylan@gmail.com>
2024-10-26 18:02:21 -0700

    fix

--- src/KnockbackEditor/events/KnockbackListener.php
+++ src/KnockbackEditor/events/KnockbackListener.php
@@ -6,7 +6,6 @@ use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\player\Player;
 use KnockbackEditor\Main;
-use pocketmine\math\Vector3;
 
 class KnockbackListener implements Listener {
     private Main $plugin;
@@ -23,16 +22,15 @@ class KnockbackListener implements Listener {
         if (!$entity instanceof Player || !$attacker instanceof Player) {
             return;
         }
+
         $world = $entity->getWorld();
         $worldName = $world->getFolderName();
         $config = $this->plugin->getKnockbackConfig();
-        
         if ($config->exists($worldName)) {
             $knockbackValues = $config->get($worldName);
             $attackDelay = intval($knockbackValues['attack-delay'] ?? 10);
             $currentTime = microtime(true);
             $playerId = $entity->getUniqueId()->toString();
-            
             if (isset($this->lastHitTime[$playerId])) {
                 $lastHit = $this->lastHitTime[$playerId];
                 $elapsedTicks = ($currentTime - $lastHit) * 20;
@@ -45,16 +43,7 @@ class KnockbackListener implements Listener {
             $x = floatval($knockbackValues["x"] ?? 0.4);
             $y = floatval($knockbackValues["y"] ?? 0.4);
             $z = floatval($knockbackValues["z"] ?? 0.4);
-            $direction = new Vector3(
-                $attacker->getPosition()->getX() - $entity->getPosition()->getX(),
-                $attacker->getPosition()->getY() - $entity->getPosition()->getY(),
-                $attacker->getPosition()->getZ() - $entity->getPosition()->getZ()
-            );
-            $direction = $direction->normalize();
-            $knockbackVector = new Vector3($direction->x * $x, $y, $direction->z * $z);
-            $entity->setMotion(
-                $entity->getMotion()->add($knockbackVector->x, $knockbackVector->y, $knockbackVector->z)
-            );
+            $event->setKnockBack($x, $y, $z);
         }
     }
 }
